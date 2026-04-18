616a34f84315fe77b54f5ac7301e5fb82a7fe430
sftdst <bambadiaw93@gmail.com>
2026-04-18 17:36:28 +0000

    delete

--- ticm-website/backend/app/Models/Certification.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-class Certification extends Model
-{
-    protected $fillable = ['code','title','category','issuer','date_obtained','expiration_date','certificate_file'];
-}
--- ticm-website/backend/app/Models/Reference.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
-class Reference extends Model
-{
-    protected $fillable = ['client_name','project_name','location','period','tag','description','image','result','category'];
-}
